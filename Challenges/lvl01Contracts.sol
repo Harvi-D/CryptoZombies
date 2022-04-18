39842688af9a1c0fe6581defda0f6ab70aba6f66
Dylan Harville <dr.dyloh@gmail.com>
2022-04-17 23:56:21 -0600

    math operator challenge

--- Challenges/lvl01Contracts.sol
+++ Challenges/lvl01Contracts.sol
@@ -5,4 +5,6 @@ pragma solidity >=0.8.13; // solidity version
 contract ZombieFactory {
     //make a 16 digit variable for zombie appearence
     uint256 dnaDigits = 16;
+    //ensure Zombie's DNA is only 16 characters
+    uint256 dnaModulus = 10**dnaDigits;
 }
